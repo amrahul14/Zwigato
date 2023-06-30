94a5eb4f83734488439275987da6270723d6e8b1
Rahul <rahul15kumar9708@gmail.com>
2023-06-30 11:45:58 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { Badge } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import Modal from "../Modal";
+import { useState } from "react";
+import Cart from "../screens/Cart";
+import { useCart } from "./ContextReducer";
+
+function Navbar() {
+  
+
+  const [cartView , setCartView] = useState(false);
+  let data = useCart();
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  };
+  return (
+    <div style={{ height: "80px" }}>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-info">
+        <div className="container-fluid">
+          <Link className="navbar-brand fs-1 fst-italic" to="/">
+            ZwiGaTo
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto mb-2">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active fs-5"
+                  aria-current="page"
+                  to="/"
+                >
+                  Home
+                </Link>
+              </li>
+              {localStorage.getItem("authToken") ? (
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active fs-5  "
+                    aria-current="page"
+                    to="/"
+                  >
+                    My Orders
+                  </Link>
+                </li>
+              ) : (
+                ""
+              )}
+            </ul>
+            {!localStorage.getItem("authToken") ? (
+              <div className="d-flex">
+                <Link className="btn bg-white text-info mx-1 " to="/login">
+                  Login
+                </Link>
+                <Link className="btn bg-white text-info mx-1" to="/createuser">
+                  SignUp
+                </Link>
+              </div>
+            ) : (
+              <div>
+                <div className="btn bg-danger text-white fw-bold mx-2" onClick={()=>{
+                  setCartView(true)
+                }}>My Cart {" "}
+                  <Badge pill bg="info" >{data.length}</Badge>
+                </div>
+                {cartView ? <Modal onClose={()=>setCartView(false)}><Cart/></Modal> : null}
+                <div
+                  className="btn bg-danger text-white fs-6 fw-bold mx-2"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/screens/Home.js
@@ -0,0 +1,165 @@
+import React, { useEffect, useReducer, useState } from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+
+function Home() {
+  const [search, setSearch] = useState("");
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+    let response = await fetch("http://localhost:8000/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    response = await response.json();
+    setFoodItem(response[0]);
+    setFoodCat(response[1]);
+    // console.log(response[0],response[1]);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+          style={{ objectFit: "contain !important" }}
+        >
+          <div className="carousel-inner" id="carousel">
+            <div className="carousel-caption" style={{ zIndex: "10" }}>
+              <div className="d-flex justify-content-center">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+                {/* <button className="btn btn-outline-info text-white bg-info" type="submit">Search</button> */}
+              </div>
+            </div>
+
+            <div className="carousel-item active">
+              <img
+                src="http://source.unsplash.com/random/900x700/?burger"
+                className="d-block w-100"
+                style={{
+                  filter: "brightness(40%)",
+                  height: "320px",
+                  objectFit: "cover",
+                }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="http://source.unsplash.com/random/900x700/?pastry"
+                className="d-block w-100"
+                style={{
+                  filter: "brightness(40%)",
+                  height: "320px",
+                  objectFit: "cover",
+                }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="http://source.unsplash.com/random/900x700/?barbeque"
+                className="d-block w-100"
+                style={{
+                  filter: "brightness(40%)",
+                  height: "320px",
+                  objectFit: "cover",
+                }}
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        {foodCat !== []
+          ? foodCat.map((data) => {
+              return (
+                <div className="row mb-3">
+                  <div key={data._id} className="fs-3 m-3">
+                    {data.CategoryName}
+                  </div>
+                  <hr />
+                  {foodItem !== [] ? (
+                    foodItem
+                      .filter(
+                        (item) =>
+                          item.CategoryName === data.CategoryName &&
+                          item.name.toLowerCase().includes(search.toLowerCase())
+                      )
+                      .map((filterItems) => {
+                        return (
+                          <div
+                            key={filterItems._id}
+                            className="col-12 col-md-6 col-lg-4"
+                          >
+                            <Card
+                            foodItem = {filterItems} 
+                            options={filterItems.options[0]}
+                            ></Card>
+                          </div>
+                        );
+                      })
+                  ) : (
+                    <div>no such data found</div>
+                  )}
+                </div>
+              );
+            })
+          : ""}
+      </div>
+      <div className="d-flex justify-content-center ">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
